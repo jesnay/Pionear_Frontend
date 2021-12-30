9ae09e23bea40054589051ef19f82a8672967f54
franziska.dudichum <stfrdudi@stud.h-da.de>
2021-12-30 16:49:53 +0100

    Create rules.js

---
+++ rules.js
@@ -0,0 +1,24 @@
+/*-----------------------------------------NAMING-----------------------------------------*/
+/**
+ * ENGLISH
+ ** file --> lowercase letters except for Class-Files
+ ** class --> uppercase first letter
+ ** function --> all lowercase letters
+ ** variable --> all lowercase letters
+ **/
+/*-----------------------------------------COMMENTS-----------------------------------------*/
+
+/**
+ * normal comments (MyMethod)
+ ** important (Important information is highlighted)
+ *! super important (Depricated method, do not use)
+ *? Questions (Should this method be exposed in the public API?)
+ *TODO obviously to dos [2] (refactor this method so that it conforms to the API)
+ *@param myParam (The parameter for this method)
+ **/
+
+// xxx
+//* xxx
+//! xxx
+//? xxx
+//TODO xxx

73192341f9cb55106c65191d4163fc94d971fbb9
stjesnay <jennifer.snay@stud.h-da.de>
2022-01-06 15:05:32 +0100

    ersten Beispiele Datenbankabfrage

--- classes/user.js
+++ classes/user.js
@@ -1,7 +1,12 @@
 import db from "../database.js";
 
 export default class User {
-  constructor() {}
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+    this.level = 1;
+    this.id = this.getID();
+  }
 
   all() {
     db.all("SELECT * FROM users", [], (err, rows) => {
@@ -10,5 +15,39 @@ export default class User {
         console.log(row);
       });
     });
+  }
+
+  insert() {
+    const sql = db.prepare(
+      "INSERT INTO users (name, email,level) VALUES (?, ?, ?)"
+    );
+    sql.run(this.name, this.email, this.level);
+    sql.finalize();
+  }
+
+  updateLevel(name) {
+    this.level++;
+    const sql = db.prepare("UPDATE users SET level= ? WHERE name=?");
+    sql.run(this.level, name);
+    sql.finalize();
+  }
+
+  getID() {
+    //*? Was ist der Unterschied von den beiden Versionen? Welches ist besser?
+    db.all(`SELECT MAX(id) AS id FROM users`, [], (err, rows) => {
+      if (err) return console.error(err.message);
+      rows.forEach((row) => {
+        this.id = row.id;
+        console.log(this.id);
+      });
+    });
+
+    /*const row = db.get(`SELECT MAX(id) AS id FROM users`, function (err, row) {
+      if (row) {
+        console.log(row.id);
+      } else {
+      }
+    });*/
+    return this.id;
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 import User from "./classes/user.js";
 
-let user = new User();
+let user = new User("Jennifer Snay", "jennifer@web.de");
 
+//user.updateLevel(user.name);
+//user.insert();
+//user.all();
+//user.getID();
 user.all();

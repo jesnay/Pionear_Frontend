617a51fa23b3b3c09a8c25fe505fdddfcaa5d546
stjesnay <jennifer.snay@stud.h-da.de>
2022-01-06 15:28:39 +0100

    hinzugefügte Kommentare

--- classes/user.js
+++ classes/user.js
@@ -1,14 +1,15 @@
-import db from "../database.js";
+import db from "../database.js"; //* Die Datenbank wird in database.js connected und kann somit genutzt werden durch import
 
 export default class User {
   constructor(name, email) {
     this.name = name;
     this.email = email;
-    this.level = 1;
-    this.id = this.getID();
+    this.level = 1; //* Level ist Standartmäßig 1
+    this.id = this.getID(); //* Die ID wird in der Datenbank automatisch vergeben (immer um eins hochgezählt vom letzten). Hier speichern wir diese ID in usnerer Variable
   }
 
   all() {
+    //* Alle Daten aus der Tabelle users werden ausgegeben
     db.all("SELECT * FROM users", [], (err, rows) => {
       if (err) return console.error(err.message);
       rows.forEach((row) => {
@@ -18,6 +19,7 @@ export default class User {
   }
 
   insert() {
+    //* Hier wird ein neues User in die Datenbank eingetragen
     const sql = db.prepare(
       "INSERT INTO users (name, email,level) VALUES (?, ?, ?)"
     );
@@ -26,6 +28,7 @@ export default class User {
   }
 
   updateLevel(name) {
+    //* Hier steigt der User um ein Level
     this.level++;
     const sql = db.prepare("UPDATE users SET level= ? WHERE name=?");
     sql.run(this.level, name);

145d80b56840ae46ffedd12dc8ac212200b06f11
franziska.dudichum <stfrdudi@stud.h-da.de>
2022-01-27 12:16:59 +0100

    added slider file

---
+++ frontend/src/components/Slide/SimpleSlider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SimpleSlider() {
+  return <div></div>;
+}
+
+export default SimpleSlider;
--- frontend/src/pages/StationMap/StationMap.jsx
+++ frontend/src/pages/StationMap/StationMap.jsx
@@ -1,10 +1,7 @@
-//*Folgende Google Map Integration funktioniert nur auf Jennifers Rechner. Die Google Map API funktioniert nur kostenpflichtig auf anderen Rechnern.
-
 import React from "react"; //imrc shortcut
 import "./StationMap.css";
 import Map from "../../assets/images/Map.png";
-// import Map from "google-map-react";
-// import p3Style from "./MapStyle";
+// import SimpleSlider from "../../components/Slide/SimpleSlider";
 
 function StationMap() {
   //ffc shortcut
@@ -15,17 +12,23 @@ function StationMap() {
       </div>
       <div className="container">
         <div className="circle"></div>
-        {/* <div style={{ width: "100%", height: "550px" }}>
-        <Map
-          bootstrapURLKeys={{ key: "hier Api key einfügen" }}
-          defaultZoom={15}
-          options={{ styles: p3Style }}
-          defaultCenter={{ lat: 49.40989504148873, lng: 8.705914587125145 }}
-        />
-      </div> */}
       </div>
     </div>
   );
 }
 
 export default StationMap;
+
+//*Folgende Google Map Integration funktioniert nur auf Jennifers Rechner. Die Google Map API funktioniert nur kostenpflichtig auf anderen Rechnern.
+
+// import Map from "google-map-react";
+// import p3Style from "./MapStyle";
+
+//  <div style={{ width: "100%", height: "550px" }}>
+//         <Map
+//           bootstrapURLKeys={{ key: "hier Api key einfügen" }}
+//           defaultZoom={15}
+//           options={{ styles: p3Style }}
+//           defaultCenter={{ lat: 49.40989504148873, lng: 8.705914587125145 }}
+//         />
+//       </div>

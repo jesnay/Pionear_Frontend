34891f1e49076832a1d42c4d48a80778866c969f
stjesnay <jennifer.snay@stud.h-da.de>
2022-02-10 11:56:30 +0100

    questTextBox

---
+++ frontend/src/components/QuestTextBox/QuestTextBox.jsx
@@ -0,0 +1,24 @@
+function QuestTextBox() {
+  let [input, setInput] = React.useState(``);
+  return (
+    <div>
+      <div>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label className="formLabel">Mögliche Affordanz</Form.Label>
+            <Form.Control
+              placeholder="z.B. Informationen liefern"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </Form.Group>
+        </Form>
+      </div>
+      <div>
+        <p>{input}</p>
+      </div>
+    </div>
+  );
+}
+
+export default QuestTextBox;

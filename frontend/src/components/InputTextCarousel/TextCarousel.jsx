8b4d11d139c3ef8cec026b42879e4b8c09143f59
stjesnay <jennifer.snay@stud.h-da.de>
2022-02-14 09:15:05 +0100

    changable text in components

--- frontend/src/components/InputTextCarousel/TextCarousel.jsx
+++ frontend/src/components/InputTextCarousel/TextCarousel.jsx
@@ -2,77 +2,43 @@ import "./TextCarousel.css";
 import React from "react";
 import { Carousel } from "react-bootstrap";
 
-function TextCarousel() {
+function TextCarousel({
+  title,
+  paragraph1,
+  paragraph2,
+  paragraph3,
+  paragraph4,
+  paragraph5,
+  paragraph6,
+  paragraph7,
+}) {
   return (
     <div>
       <div>
         <Carousel interval={null} className="InfoText">
           <Carousel.Item>
             <p>
-              <strong>Praktiken</strong>
+              <strong>{title}</strong>
             </p>
-            <p>
-              Hier sehen wir das wohl prestigeträchtigste Artefakt der
-              Universitätsbibliothek, den Codex Manesse. Diese
-              Liederhandschriftensammlung entstand im frühen 14. Jahrhundert -
-              unserem Mittelalter. Dabei wurde die bis dato nur mündlich
-              überlieferte Tradition des deutschen Minnesanges verschriftlicht.
-            </p>
+            <p>{paragraph1}</p>
           </Carousel.Item>
           <Carousel.Item>
-            <p>
-              Mehr über diese Verschriftlichung, ihren Kontext und die
-              zugrundeliegenden Absichten zu erfahren gehört zu den
-              Fragestellungen des Sonderforschungsbereichs. Wieso hielten die
-              Sammler der Lieder deren Niederschrift plötzlich für notwendig?
-              Schon damals hatten die Sammler einen Anspruch auf
-              Vollständigkeit. Heute gilt der Codex
-            </p>
+            <p>{paragraph2}</p>
           </Carousel.Item>
           <Carousel.Item>
-            <p>
-              Manesse als die umfangreichste deutsche Liederhandschrift des
-              Mittelalters. Trotzdem blieben von den 426 etwa ein Drittel der
-              Seiten leer und zahlreiche wurden nur zum Teil beschrieben. Es
-              wurde also reichlich Platz gelassen. Pergament war damals sehr
-              teuer und die Herstellung eines Buches ein zeitintensiver Prozess.
-              Das gilt besonders für die vielen farben-
-            </p>
+            <p>{paragraph3}</p>
           </Carousel.Item>
           <Carousel.Item>
-            <p>
-              prächtigen Illustrationen. Genau diese Zeichnungen sind es, die
-              den Codex heute so berühmt machen. Das Buch hat eine sehr
-              wechselvolle Besitzgeschichte, die zwar nicht immer nachvollzogen
-              werden kann, aber die heutigen Länder Schweiz, Niederlande,
-              Frankreich und Deutschland umfasst, so dass man in diesem
-              Zusammenhang auch von einer „Reise“
-            </p>
+            <p>{paragraph4}</p>
           </Carousel.Item>
           <Carousel.Item>
-            <p>
-              sprechen kann. Nach Heidelberg kam der Codex Manesse 1607, wo er
-              in die ebenfalls berühmte Biblioteca Palatina aufgenommen wurde.
-              Dort blieb er jedoch nur 15 Jahre, denn 1622 stand während des
-              30jährigen Krieges die katholische Liga vor den Toren Heidelbergs
-              vor der der Codex Manesse in Sicherheit gebracht wurde und
-              schließlich über mehrere Stationen nach
-            </p>
+            <p>{paragraph5}</p>
           </Carousel.Item>
           <Carousel.Item>
-            <p>
-              Paris gelangte. Der Rest der Biblioteca Palatina wurde als Beute
-              in den Vatikan gebracht. Anschließend bemühten sich mehrere
-              deutsche Herrscher und Fürsten den Codex Manesse zurück nach
-              Heidelberg zu bringen, was allerdings erst 1888 gelang. Gerade
-              diese Bemühungen zeigen, welch hohes Prestige mit dem Besitz des
-              Buches einherging - es erlangte eine Bedeutung
-            </p>
+            <p>{paragraph6}</p>
           </Carousel.Item>
           <Carousel.Item>
-            <p>
-              über den bloßen immanenten Nutzungscharakter – dem Lesen – hinaus.
-            </p>
+            <p>{paragraph7}</p>
           </Carousel.Item>
         </Carousel>
       </div>
--- frontend/src/pages/Test/Test.jsx
+++ frontend/src/pages/Test/Test.jsx
@@ -5,10 +5,45 @@ import { Link } from "react-router-dom";
 function Test() {
   return (
     <div>
-      <div>
-        <p>Ich bin die Test.jsx</p>
-      </div>
-      <TextCarousel></TextCarousel>
+      <TextCarousel
+        title="Praktiken"
+        paragraph1="Hier sehen wir das wohl prestigeträchtigste Artefakt der
+              Universitätsbibliothek, den Codex Manesse. Diese
+              Liederhandschriftensammlung entstand im frühen 14. Jahrhundert -
+              unserem Mittelalter. Dabei wurde die bis dato nur mündlich
+              überlieferte Tradition des deutschen Minnesanges verschriftlicht."
+        paragraph2="Mehr über diese Verschriftlichung, ihren Kontext und die
+              zugrundeliegenden Absichten zu erfahren gehört zu den
+              Fragestellungen des Sonderforschungsbereichs. Wieso hielten die
+              Sammler der Lieder deren Niederschrift plötzlich für notwendig?
+              Schon damals hatten die Sammler einen Anspruch auf
+              Vollständigkeit. Heute gilt der Codex"
+        paragraph3="Manesse als die umfangreichste deutsche Liederhandschrift des
+              Mittelalters. Trotzdem blieben von den 426 etwa ein Drittel der
+              Seiten leer und zahlreiche wurden nur zum Teil beschrieben. Es
+              wurde also reichlich Platz gelassen. Pergament war damals sehr
+              teuer und die Herstellung eines Buches ein zeitintensiver Prozess.
+              Das gilt besonders für die vielen farben-"
+        paragraph4="prächtigen Illustrationen. Genau diese Zeichnungen sind es, die
+              den Codex heute so berühmt machen. Das Buch hat eine sehr
+              wechselvolle Besitzgeschichte, die zwar nicht immer nachvollzogen
+              werden kann, aber die heutigen Länder Schweiz, Niederlande,
+              Frankreich und Deutschland umfasst, so dass man in diesem
+              Zusammenhang auch von einer „Reise“"
+        paragraph5="sprechen kann. Nach Heidelberg kam der Codex Manesse 1607, wo er
+              in die ebenfalls berühmte Biblioteca Palatina aufgenommen wurde.
+              Dort blieb er jedoch nur 15 Jahre, denn 1622 stand während des
+              30jährigen Krieges die katholische Liga vor den Toren Heidelbergs
+              vor der der Codex Manesse in Sicherheit gebracht wurde und
+              schließlich über mehrere Stationen nach"
+        paragraph6="Paris gelangte. Der Rest der Biblioteca Palatina wurde als Beute
+              in den Vatikan gebracht. Anschließend bemühten sich mehrere
+              deutsche Herrscher und Fürsten den Codex Manesse zurück nach
+              Heidelberg zu bringen, was allerdings erst 1888 gelang. Gerade
+              diese Bemühungen zeigen, welch hohes Prestige mit dem Besitz des
+              Buches einherging - es erlangte eine Bedeutung"
+        paragraph7="über den bloßen immanenten Nutzungscharakter – dem Lesen – hinaus."
+      ></TextCarousel>
       <div>
         <Link to="/map">
           <button>Go to Map</button>

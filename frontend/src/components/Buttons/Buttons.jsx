2001590b230c1ad9ec05b86fe9ab2880d6396b7e
franziska.dudichum <stfrdudi@stud.h-da.de>
2022-02-19 12:18:42 +0100

    buttons added component

---
+++ frontend/src/components/Buttons/Buttons.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import styles from "./Buttons.module.css";
+
+class Buttons extends Component {
+  state = {};
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Buttons;

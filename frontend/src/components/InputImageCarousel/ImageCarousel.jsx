86c4648da70dd2cad14233d94384f66dbe67d031
stjesnay <jennifer.snay@stud.h-da.de>
2022-02-24 17:55:33 +0100

    finished Text Box Input

--- frontend/src/components/InputImageCarousel/ImageCarousel.jsx
+++ frontend/src/components/InputImageCarousel/ImageCarousel.jsx
@@ -1,15 +1,15 @@
 import styles from "./ImageCarousel.module.css";
 import React from "react";
-// import { Carousel } from "react-bootstrap";
+//import { Carousel } from "react-bootstrap";
 
-// import OwlCarousel from "react-owl-carousel";
-// import "owl.carousel/dist/assets/owl.carousel.css";
-// import "owl.carousel/dist/assets/owl.theme.default.css";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 function ImageCarousel({ img1, img2, img3 }) {
   return (
     <div className={styles.Image}>
-      {/* <OwlCarousel className="owl-theme" loop margin={-10} autoWidth center nav> */}
+      <OwlCarousel className="owl-theme" loop margin={-10} autoWidth center nav>
         <div className="item">
           <img src={img1} alt=""></img>
         </div>
@@ -19,7 +19,7 @@ function ImageCarousel({ img1, img2, img3 }) {
         <div className="item">
           <img src={img3} alt=""></img>
         </div>
-      {/* </OwlCarousel> */}
+      </OwlCarousel>
     </div>
   );
 }

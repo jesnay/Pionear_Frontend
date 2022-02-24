6e0c4b61a11a9932ba68ac57960ea1cacb29b467
stjesnay <jennifer.snay@stud.h-da.de>
2022-02-24 19:16:11 +0100

    inserted Audio button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Test from "./pages/Test/Test.jsx";
 import Map from "./pages/Map/Map.jsx";
+import Input from "./pages/Input/Input.jsx";
 // // import Blurb from "./components/Blurb/Blurb.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<Test />}></Route>
-        <Route path="/map" exact element={<Map />}></Route>
+        <Route path="/" exact element={<Input />}></Route>
+        {/*<Route path="/map" exact element={<Map />}></Route>*/}
         {/* <Route path="/blurb" exact element={<Blurb />}></Route> */}
       </Routes>
     </Router>
--- frontend/src/components/InputAudio/Audio.jsx
+++ frontend/src/components/InputAudio/Audio.jsx
@@ -1,11 +1,15 @@
-import "./Audio.module.css";
+import styles from "./Audio.module.css";
 import React from "react";
 import Audio4 from "../../assets/audio/4_Affordanz.mp3";
 
 function Audio() {
   return (
     <div>
-      <audio controls="controls">
+      <audio
+        controls="controls"
+        className={styles.Audio}
+        controlsList="noplaybackrate"
+      >
         <source src={Audio4} type="audio/ogg" />
         <source src={Audio4} type="audio/mpeg" />
         Your browser does not support the audio element.

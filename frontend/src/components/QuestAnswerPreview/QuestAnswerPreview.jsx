30a9404549d5bd97e3eea9b8ce3e93bae9d90f8c
stjesnay <jennifer.snay@stud.h-da.de>
2022-02-26 17:47:52 +0100

    answerpreview component

--- frontend/src/components/QuestAnswerPreview/QuestAnswerPreview.jsx
+++ frontend/src/components/QuestAnswerPreview/QuestAnswerPreview.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import styles from "./QuestAnswerPreview.module.css";
+import Wave from "../../assets/images/Waves/QuestWave_V2.png";
+import user0 from "../../assets/images/QuestAnswers/user0.jpg";
+import user1 from "../../assets/images/QuestAnswers/user1.jpg";
 
 function QuestAnswerPreview() {
   return (
-    <div>
-      <p>
-        Hier wird die Preview von den QuestAnswers gebaut, die dann im Input
-        importiert wird
-      </p>
+    <div
+      className={styles.AnswerPreview}
+      role="button"
+      onClick={() => {
+        console.log("hi");
+      }}
+    >
+      <div>
+        <img className="questWave" src={Wave} alt="backgroundWave" />
+        <h3>Sieh dir an wie andere die Quest gelöst haben!</h3>
+        <p>Letzte Aktivität vor 2h</p>
+      </div>
+      <div>
+        <img className="Preview1" src={user0} />
+        <img className="Preview2" src={user1} />
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Quest/Quest.jsx
+++ frontend/src/pages/Quest/Quest.jsx
@@ -6,9 +6,49 @@ import Wave from "../../assets/images/Waves/QuestWave_V2.png";
 import QuestPhotoPreview from "../../components/QuestPhotoPreview/QuestPhotoPreview.jsx";
 import userImage from "../../assets/images/Stations/4_Affordanz/quest_userImage.jpg";
 
-function Quest() {
-  let state = "Quest1";
-
+function QuestTakeImage() {
+  return (
+    <div className={styles.Quest}>
+      <div>
+        <img className="questWave" src={Wave} alt="backgroundWave" />
+      </div>
+      <div>
+        <h1 className="h1">Quest I</h1>
+        <p className="questTask1">Bewege Dich durch die Unibibliothek.</p>
+        <p className="questTask2">
+          Suche Dir ein schrifttragendes Objekt Deiner Wahl aus und mache
+          <br />
+          davon ein Foto.
+        </p>
+      </div>
+      <div className="PhotoButton">
+        <button>take pic</button>
+      </div>
+    </div>
+  );
+}
+function QuestPreviewImage() {
+  return (
+    <div className={styles.Quest}>
+      <div>
+        <img className="questWave" src={Wave} alt="backgroundWave" />
+      </div>
+      <div>
+        <h1 className="h1">Quest I</h1>
+        <p className="questTask1">Bewege Dich durch die Unibibliothek.</p>
+        <p className="questTask2">
+          Suche Dir ein schrifttragendes Objekt Deiner Wahl aus und mache
+          <br />
+          davon ein Foto.
+        </p>
+      </div>
+      <div className="PhotoPreview">
+        <QuestPhotoPreview image={userImage} />
+      </div>
+    </div>
+  );
+}
+function QuestUserInput() {
   let [input, setInput] = React.useState([]);
 
   const handleCallback = (childData) => {
@@ -27,62 +67,39 @@ function Quest() {
     }
     console.log(input);
   }
-
-  if (state === "Quest1") {
-    return (
-      <div className={styles.Quest}>
-        <div>
-          <img className="questWave" src={Wave} alt="backgroundWave" />
-        </div>
-        <div>
-          <h1 className="h1">Quest II</h1>
-          <p className="questTask1">Bewege Dich durch die Unibibliothek.</p>
-          <p className="questTask2">
-            Suche Dir ein schrifttragendes Objekt Deiner Wahl aus und mache
-            <br />
-            davon ein Foto.
-          </p>
-        </div>
-        <div className="PhotoPreview">
-          <QuestPhotoPreview image={userImage} />
-        </div>
+  return (
+    <div className={styles.Quest}>
+      <div>
+        <img className="questWave" src={Wave} alt="backgroundWave" />
       </div>
-    );
-  } else if (state === "Quest2") {
-    return (
-      <div className={styles.Quest}>
-        <div>
-          <img className="questWave" src={Wave} alt="backgroundWave" />
-        </div>
-        <div>
-          <h1 className="h1">Quest II</h1>
-          <p className="questTask1">
-            Welchen Nutzen kann die Schrift auf dem Objekt haben? Schreib Deine
-            Gedanken zu dem ausgesuchten Objekt auf.
-          </p>
-        </div>
-        <div className="UserTyping">
-          <QuestTextBox func={handleCallback} />
-        </div>
-        <div className="UserInput">
-          <QuestTextBoxInput input={input} />
-        </div>
-        <div>
-          {input.map((input) => (
-            <button
-              className={styles.deleteButton}
-              key={input.childData}
-              onClick={() => {
-                handleRemove(input, input.childData);
-              }}
-            >
-              1
-            </button>
-          ))}
-        </div>
+      <div>
+        <h1 className="h1">Quest II</h1>
+        <p className="questTask1">
+          Welchen Nutzen kann die Schrift auf dem Objekt haben? Schreib Deine
+          Gedanken zu dem ausgesuchten Objekt auf.
+        </p>
       </div>
-    );
-  }
+      <div className="UserTyping">
+        <QuestTextBox func={handleCallback} />
+      </div>
+      <div className="UserInput">
+        <QuestTextBoxInput input={input} />
+      </div>
+      <div>
+        {input.map((input) => (
+          <button
+            className={styles.deleteButton}
+            key={input.childData}
+            onClick={() => {
+              handleRemove(input, input.childData);
+            }}
+          >
+            1
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Quest;
+export { QuestTakeImage, QuestPreviewImage, QuestUserInput };

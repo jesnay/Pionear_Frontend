5cf79ed6ed98ba626ca7c473ec9a349cf441476c
franziska.dudichum <stfrdudi@stud.h-da.de>
2022-02-27 15:26:56 +0100

    update

--- frontend/src/pages/Map/Map.jsx
+++ frontend/src/pages/Map/Map.jsx
@@ -7,23 +7,20 @@ import MapImage from "../../assets/images/Map.png";
 import locationInactive from "../../assets/images/Icons/location-inactive.png";
 import locationActive from "../../assets/images/Icons/location-active.png";
 
-
 function Map() {
   return (
     <div className={styles.Map}>
-      <img className="MapImage" src={MapImage} alt="MapImage" />
-
-
-      <div className="locationContainer">
-        <img className="inactive1" src={locationInactive} alt="" />
-        <img className="inactive2" src={locationInactive} alt="" />
-        <img className="active" src={locationActive} alt="" />
+      <div className="Brightness">
+        <img className="MapImage" src={MapImage} alt="MapImage" />
+        <div className="locationContainer">
+          <img className="inactive1" src={locationInactive} alt="" />
+          <img className="inactive2" src={locationInactive} alt="" />
+          <img className="active" src={locationActive} alt="" />
+        </div>
+        <div className="circleContainer">
+          <div className="circle"></div>
+        </div>
       </div>
-
-      <div className="circleContainer">
-        <div className="circle"></div>
-      </div>
-
       <div className="Blurb">
         <Blurb />
       </div>
